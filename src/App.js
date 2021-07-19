2a464d8c8aa68a65299e0bdd4398ede1a7263bbf
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-19 16:51:06 +0200

    Correct logic component & variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Landing from "./pages/Landing";
 import './styles/global.scss'
+import NavHeader from "./components/organisms/NavHeader";
+import NavSidebar from "./components/organisms/NavSidebar";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div className="App">
-     <Landing/>
+        <NavHeader/>
+        <main>
+            <NavSidebar/>
+            <Landing />
+        </main>
     </div>
   );
 }
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import NavHeader from "../../components/organisms/NavHeader";
-import NavSidebar from "../../components/organisms/NavSidebar";
 
 export default function Landing() {
 
     return (
         <div className="landing">
-            <NavHeader/>
-            <main>
-                <NavSidebar/>
-                <div className="content">Cc</div>
-            </main>
+            Coucou
         </div>
     )
 }
