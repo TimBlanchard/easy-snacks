11fac8078175fc402fff6019261b844e6a764302
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-19 16:54:07 +0200

    Class default main content

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Landing() {
 
     return (
-        <div className="landing">
+        <div className="main-app landing">
             Coucou
         </div>
     )
