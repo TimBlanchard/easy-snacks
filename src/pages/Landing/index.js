267319c8f61abbe25106b6dfae7cffae0ecde978
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-20 12:22:54 +0200

    Summary architecture atomic

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Error from '../../components/organisms/Error'
+import Summary from '../../components/organisms/Summary'
 
 export default function Landing() {
 
     return (
         <div className="main-app landing">
             <Error />
+            <Summary />
         </div>
     )
 }
