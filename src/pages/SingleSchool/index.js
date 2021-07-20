9085551e6d52db56f1a28500947801fd68b7531c
TimBlanchard <36016548+TimBlanchard@users.noreply.github.com>
2021-07-20 23:05:17 +0200

    add graph chart js

--- src/pages/SingleSchool/index.js
+++ src/pages/SingleSchool/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Summary from '../../components/organisms/Summary'
+import SchoolRevenue from "../../components/organisms/SchoolRevenue";
 
 export default function SingleSchool() {
 
@@ -7,6 +8,7 @@ export default function SingleSchool() {
         <div className="main-app single-school">
             <p>single school</p>
             <Summary />
+            <SchoolRevenue/>
         </div>
     )
 }
