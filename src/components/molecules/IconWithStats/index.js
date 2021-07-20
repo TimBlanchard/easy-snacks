acab98f6702512c1232863b25be25c169548e889
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-20 14:49:10 +0200

    Summary landing

--- src/components/molecules/IconWithStats/index.js
+++ src/components/molecules/IconWithStats/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './index.scss'
 import BoxIcon from '../../atoms/BoxIcon'
+import NumberFormat from 'react-number-format';
 
-export default function IconWithStats({text, type}) {
+export default function IconWithStats({title, period, value, type}) {
 
     return (
         <div className="iconWithStats">
             <BoxIcon type={type} />
-            {text}
+            <div className="iconWithStats__content">
+                <p className="iconWithStats__content__title">{title}<span> {period}</span></p>
+                <NumberFormat className="iconWithStats__content__value" value={value[0]} displayType={'text'} thousandSeparator={' '} suffix={value[1]} />
+            </div>
         </div>
     )
 }
--- src/components/organisms/Summary/index.js
+++ src/components/organisms/Summary/index.js
@@ -6,10 +6,10 @@ export default function Summary() {
 
     return (
         <div className="summary box-default">
-            <IconWithStats text="Coucou" type="home" />
-            <IconWithStats text="Cava" type="search" />
-            <IconWithStats text="Trkl" type="activity" />
-            <IconWithStats text="Ok" type="search" />
+            <IconWithStats title='Revenus' period='/1 jour' value={['3487', '€', 'suffix']} type="home" />
+            <IconWithStats title='Revenus' period='/1 semaine' value={['12984', '€', 'suffix']} type="search" />
+            <IconWithStats title='Revenus' period='/1 mois' value={['48290', '€', 'suffix']} type="activity" />
+            <IconWithStats title='Revenus' period='/1 an' value={['508488', '€', 'suffix']} type="search" />
         </div>
     )
 }
