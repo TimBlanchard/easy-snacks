5c25bf132d6f49dc9ccae0af948638c02f6d9336
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-26 18:03:47 +0200

    back link school

--- src/components/organisms/Error/index.js
+++ src/components/organisms/Error/index.js
@@ -7,7 +7,7 @@ export default function Error() {
     return (
         <div className="error box-default">
             <div className="error__left">
-                <img className="error__icon" src="/assets/images/icons/warning.svg"/>
+                <img className="error__icon" src="/assets/images/icons/warning-red.svg"/>
                 <div className="error__text">
                     <p className="error__text__title">3 de vos distributeurs sont en pannes</p>
                     <p className="error__text__information">Une action immédiate de votre part est requise</p>
