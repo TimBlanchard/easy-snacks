edbe0f36ce2446d291f738f900dbb66df3971de5
Mathis Angibaud <m.angibaudmathis@gmail.com>
2021-07-26 17:59:59 +0200

    last components ecole

--- src/components/organisms/Alert/index.js
+++ src/components/organisms/Alert/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './index.scss'
 
-export default function Alert() {
+export default function Alert({ type, title, description }) {
     return (
         <div className="alert">
-            <div className="alert__box">
-                <img className="alert__box__icon" src="/assets/images/icons/warning.svg"/>
+            <div className={"alert__box alert__box__"+type}>
+                <img className="alert__box__icon" src={"/assets/images/icons/warning-"+type+".svg"} />
+            </div>
+            <div className="alert__content">
+                <p className="name">{title}</p>
+                <p>{description}</p>
             </div>
         </div>
     )
--- src/components/organisms/AlertHistoric/index.js
+++ src/components/organisms/AlertHistoric/index.js
@@ -7,7 +7,9 @@ export default function AlertHistoric() {
     return (
         <div className="alert-historic box-default">
             <HeaderBox title="Historique des alertes" montant="3" />
-            <Alert />
+            <Alert type="red" title="#652" description="Distributeur en panne" />
+            <Alert type="orange" title="#152" description="Distributeur plein" />
+            <Alert type="red" title="#32" description="Distributeur en panne" />
         </div>
     )
 }
