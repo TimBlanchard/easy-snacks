166cd1bcbdc1827fc4e4dbfa3f7abec598086bd8
TimBlanchard <36016548+TimBlanchard@users.noreply.github.com>
2021-07-26 15:02:14 +0200

    test git

---
+++ dashboard-app/dashboard-app/src/App.js
@@ -0,0 +1,21 @@
+import './styles/global.scss'
+import NavHeader from "./components/organisms/NavHeader";
+import NavSidebar from "./components/organisms/NavSidebar";
+import Landing from "./pages/Landing";
+import { Route } from "wouter";
+import SingleSchool from "./pages/SingleSchool";
+
+function App() {
+  return (
+    <div className="App">
+        <NavHeader/>
+        <main>
+            <NavSidebar/>
+            <Route path="/" component={Landing}/>
+            <Route path="/schools/:name" component={SingleSchool}/>
+        </main>
+    </div>
+  );
+}
+
+export default App;
